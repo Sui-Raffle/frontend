91e18bb0c612fac669797c4ac803a1111f260dfa
EasonC13 <gtes9950406@gmail.com>
2023-07-22 13:41:17 +0800

    fix: li

--- src/components/CreateCoinRaffle.jsx
+++ src/components/CreateCoinRaffle.jsx
@@ -315,7 +315,7 @@ export default function CreateCoinRaffle() {
                           .click();
                       };
                       return (
-                        <li key={index}>
+                        <li key={`${index}`}>
                           <button
                             className='block w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
                             onClick={handleOnClick}
@@ -331,7 +331,7 @@ export default function CreateCoinRaffle() {
                         </li>
                       );
                     } else {
-                      return <></>;
+                      return <li key={`${index}`}></li>;
                     }
                   })}
                 </ul>
