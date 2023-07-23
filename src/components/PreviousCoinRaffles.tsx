0093f1e138a58190f04b70b9ac2234dce20f4e02
EasonC13 <gtes9950406@gmail.com>
2023-07-24 00:10:18 +0800

    add: display NFT Raffles

--- src/components/PreviousCoinRaffles.tsx
+++ src/components/PreviousCoinRaffles.tsx
@@ -4,6 +4,7 @@ import { useWalletKit } from '@mysten/wallet-kit';
 import getSuiProvider from '../lib/getSuiProvider';
 import { CoinMetadatas } from '../lib/config';
 import { moveCallSettleCoinRaffle } from '../lib/moveCallSettleCoinRaffle';
+import { moveCallSettleNFTRaffle } from '../lib/moveCallSettleNFTRaffle';
 import { RafflePackageId } from '../lib/config';
 import { getNetwork } from '../lib/getNetwork';
 import { updateCoinMetadatas } from '../lib/updateCoinMetadatas';
@@ -14,58 +15,120 @@ export function PreviousCoinRaffles() {
   const [raffles, setRaffles] = React.useState([]);
   const [raffleFetched, setRaffleFetched] = React.useState(false);
   const [coinMetadatasReady, setCoinMetadatasReady] = React.useState(false);
-  const [actionPending, setActionPending] = React.useState(Object());
+  const [listedNftIds, setListedNftIds] = React.useState([]);
+  const [listedNftObjs, setListedNftObjs] = React.useState<any>({});
+
+  React.useEffect(() => {
+    if (
+      walletKit &&
+      walletKit.currentAccount &&
+      Object.keys(listedNftObjs).length == 0
+    ) {
+      (async () => {
+        let network = getNetwork(walletKit);
+        let provider = getSuiProvider(network);
+        let NftObjs = Object();
+        let res: any = {};
+        do {
+          res = await provider?.multiGetObjects({
+            ids: listedNftIds,
+            options: { showContent: true, showDisplay: true },
+          });
+          for (let index = 0; index < listedNftIds.length; index++) {
+            const id = listedNftIds[index];
+            const obj = res[index].data;
+            NftObjs[id] = obj;
+          }
+        } while (res.hasNextPage);
+        setListedNftObjs(NftObjs);
+      })();
+    }
+  }, [listedNftIds, walletKit, listedNftObjs]);
+
+  React.useEffect(() => {
+    let nftObjIds: any = [];
+    let nft_raffles = raffles.filter((raffle: any) =>
+      raffle.type.includes('nft_raffle::NFT_Raffle')
+    );
+
+    for (let index = 0; index < nft_raffles.length; index++) {
+      let raffle: any = nft_raffles[index];
+      nftObjIds = nftObjIds.concat(
+        raffle.createdEventParsedJson.reward_nft_ids
+      );
+    }
+    setListedNftIds(nftObjIds);
+  }, [raffles]);
 
   React.useEffect(() => {
     if (walletKit && walletKit.currentAccount) {
-      let network = getNetwork(walletKit);
-      let provider = getSuiProvider(network);
-
-      provider
-        ?.queryEvents({
-          query: {
-            MoveEventType: `${RafflePackageId[network]}::raffle::CoinRaffleCreated`,
-          },
-        })
-        .then(async (events) => {
-          if (events.hasNextPage) {
-            ('');
+      let run = async () => {
+        let network = getNetwork(walletKit);
+        let provider = getSuiProvider(network);
+        let datas: any = [];
+        let cursor = '';
+        do {
+          let res: any = await provider?.queryEvents({
+            query: {
+              MoveEventType: `${RafflePackageId[network]}::raffle::CoinRaffleCreated`,
+            },
+          });
+          datas = datas.concat(res.data);
+          if (res.hasNextPage) {
+            cursor = res.nextCursor;
             // 等有夠多再來寫比較快
+          } else {
+            cursor = '';
           }
-          let raffeObjIds = events.data.map(
-            (event: any) => event.parsedJson.raffle_id
-          );
-          let RafflesEventData: any = {};
-          for (let index = 0; index < events.data.length; index++) {
-            const event: any = events.data[index];
-
-            RafflesEventData[event.parsedJson.raffle_id] = {
-              timestampMs: event.timestampMs,
-              prizeAmount: event.parsedJson.prizeAmount,
-            };
+        } while (cursor);
+        do {
+          let res: any = await provider?.queryEvents({
+            query: {
+              MoveEventType: `${RafflePackageId[network]}::nft_raffle::NftRaffleCreated`,
+            },
+          });
+          datas = datas.concat(res.data);
+          if (res.hasNextPage) {
+            cursor = res.nextCursor;
+            // 等有夠多再來寫比較快
+          } else {
+            cursor = '';
           }
-          let network = getNetwork(walletKit);
-          let provider = getSuiProvider(network);
+        } while (cursor);
+        let raffeObjIds = datas.map((event: any) => event.parsedJson.raffle_id);
+        let RafflesEventData: any = {};
+        for (let index = 0; index < datas.length; index++) {
+          const event: any = datas[index];
 
-          let res = await provider?.multiGetObjects({
-            ids: raffeObjIds,
-            options: { showContent: true },
-          });
-          let raffles: any = res?.map((item: any) => {
-            return {
-              ...RafflesEventData[item.data.objectId],
-              ...item.data.content,
-              ...item.data.content.fields,
-            };
-          });
+          RafflesEventData[event.parsedJson.raffle_id] = {
+            timestampMs: event.timestampMs,
+            createdEventParsedJson: event.parsedJson,
+          };
+        }
 
-          setRaffles(raffles);
-          let raffleTypes = raffles.map((raffle: any) =>
-            getRaffleCoinType(raffle.type)
-          );
-          await updateCoinMetadatas(raffleTypes, walletKit);
-          setCoinMetadatasReady(true);
+        let res = await provider?.multiGetObjects({
+          ids: raffeObjIds,
+          options: { showContent: true },
         });
+        let raffles: any = res?.map((item: any) => {
+          return {
+            ...RafflesEventData[item.data.objectId],
+            ...item.data.content,
+            ...item.data.content.fields,
+          };
+        });
+        raffles.sort((a: any, b: any) => {
+          return Number(b.timestampMs) - Number(a.timestampMs);
+        });
+
+        setRaffles(raffles);
+        let raffleTypes = raffles.map((raffle: any) =>
+          getRaffleCoinType(raffle.type)
+        );
+        await updateCoinMetadatas(raffleTypes, walletKit);
+        setCoinMetadatasReady(true);
+      };
+      run();
     }
   }, [walletKit]);
   // if (createRaffleEvents.length) {
@@ -94,7 +157,7 @@ export function PreviousCoinRaffles() {
                 Creator
               </th>
               <th className='py-3 px-6 bg-gray-50 text-left text-xs font-medium text-gray-500  tracking-wider'>
-                Total Reward
+                Prizes
               </th>
               <th className='py-3 px-6 bg-gray-50 text-left text-xs font-medium text-gray-500  tracking-wider'>
                 Participants
@@ -123,25 +186,64 @@ export function PreviousCoinRaffles() {
                   return <></>;
                 }
               };
-              let prizeField = () => {
-                if (coinMetadatasReady) {
-                  return (
-                    <span>
-                      {raffle.prizeAmount / 10 ** coinMetadata.decimals}{' '}
-                      {icon()}
-                      {coinMetadata.name}
-                    </span>
-                  );
-                } else {
-                  return <span></span>;
-                }
-              };
-              let handleSettleRaffle = async () => {
+              let prizeField;
+              let settleFunction = moveCallSettleCoinRaffle;
+              if (raffle.type.includes('::Raffle')) {
+                prizeField = () => {
+                  if (coinMetadatasReady) {
+                    return (
+                      <span>
+                        {raffle.createdEventParsedJson.prizeAmount /
+                          10 ** coinMetadata.decimals}{' '}
+                        {icon()}
+                        {coinMetadata.name}
+                      </span>
+                    );
+                  } else {
+                    return <span></span>;
+                  }
+                };
+                settleFunction = moveCallSettleCoinRaffle;
+              } else if (raffle.type.includes('::NFT_Raffle')) {
+                prizeField = () => {
+                  if (Object.keys(listedNftObjs).length) {
+                    let network = getNetwork(walletKit);
+                    console.log(raffle.createdEventParsedJson.reward_nft_ids);
+                    return (
+                      <div className='overflow-x-auto'>
+                        {raffle.createdEventParsedJson.reward_nft_ids.length}{' '}
+                        NFT(s):
+                        {raffle.createdEventParsedJson.reward_nft_ids.map(
+                          (id: string, index: number) => {
+                            console.log('listedNftObjs', listedNftObjs[id]);
+                            return (
+                              <a
+                                key={index}
+                                href={`https://suiexplorer.com/object/${id}?network=https%3A%2F%2Fsui-${network}-endpoint.blockvision.org`}
+                                target='_blank'
+                              >
+                                <img
+                                  src={listedNftObjs[id].display.data.image_url}
+                                  className='h-7 inline-block rounded'
+                                ></img>
+                              </a>
+                            );
+                          }
+                        )}
+                      </div>
+                    );
+                  }
+                  return <span>NFTs</span>;
+                };
+                settleFunction = moveCallSettleNFTRaffle;
+              }
+              let handleSettleCoinRaffle = async () => {
                 actionPending[settleRaffleButtonId] = true;
                 setActionPending({
                   ...actionPending,
                 });
-                let result = await moveCallSettleCoinRaffle({
+
+                let result = await settleFunction({
                   raffleObjId: raffle.id.id,
                   walletKit,
                 });
@@ -154,6 +256,7 @@ export function PreviousCoinRaffles() {
                   ...actionPending,
                 });
               };
+
               let handleViewRaffle = () => {
                 let network =
                   walletKit.currentAccount.chains[0].split('sui:')[1];
@@ -168,7 +271,7 @@ export function PreviousCoinRaffles() {
                   return (
                     <Button
                       className='bg-green-500 hover:bg-green-700 rounded-lg text-white'
-                      onClick={handleSettleRaffle}
+                      onClick={handleSettleCoinRaffle}
                       id={settleRaffleButtonId}
                       disabled={actionPending[settleRaffleButtonId]}
                       style={
@@ -231,9 +334,11 @@ export function PreviousCoinRaffles() {
                     {prizeField()}
                   </td>
                   <td className='py-4 px-6 border-b border-gray-200'>
-                    {raffle.participants.length}
+                    {raffle.participants.length + raffle.winners.length}
                   </td>
-                  <td className='py-4 px-6 border-b border-gray-200'>{`${raffle.winnerCount}/${raffle.participants.length}`}</td>
+                  <td className='py-4 px-6 border-b border-gray-200'>{`${
+                    raffle.winnerCount
+                  }/${raffle.participants.length + raffle.winners.length}`}</td>
                   <td className='py-4 px-6 border-b border-gray-200'>
                     {raffleActions()}
                   </td>
